727c51dc12f3fa68b21e1661ece7cf28baca4003
Richard Leinfellner <richard@modulo17.com>
2018-02-28 11:08:14 +0000

    For Class

---
+++ Assets/SeeingThings/NavAgentTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentTest : MonoBehaviour {
+
+    NavMeshAgent mAgent;
+
+    public  GameObject Target;
+
+	// Use this for initialization
+	void Start () {
+        mAgent = GetComponent<NavMeshAgent>();
+        if (Target != null) {
+            mAgent.destination = Target.transform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
